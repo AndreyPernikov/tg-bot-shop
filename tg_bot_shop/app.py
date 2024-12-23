0e0cb0615bfa74ece784f05e0220cd8d6109633e
AndreyPernikov <Andrey.pernikov@mail.ru>
2024-12-23 13:19:28 +0300

    l.2

--- tg_bot_shop/app.py
+++ tg_bot_shop/app.py
@@ -15,16 +15,12 @@ async def start_cmd(message: types.Message):
 
 @dp.message()
 async def echo(message: types.Message):
-    text = message.text
-    if text in ['Привет', 'привет', 'hi', 'hello']:
-        await message.answer('И тебе привет')
-    elif text in ['Пока', 'пока', 'пакеда', 'До свидания']:
-        await message.answer('И тебе пока')
-    else:
-        await message.answer(message.text)
+    await message.answer(message.text)  # Упомянуть пользователя
+    await message.reply(message.text)
 
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 
