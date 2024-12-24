b119c473af2bb0a23eb43c6f6132002c34abc1fa
AndreyPernikov <Andrey.pernikov@mail.ru>
2024-12-24 17:31:05 +0300

    l.3

--- tg_bot_shop/app.py
+++ tg_bot_shop/app.py
@@ -1,27 +1,26 @@
 import asyncio
+import os
 
+from dotenv import find_dotenv, load_dotenv
 from aiogram import Bot, types, Dispatcher
 from aiogram.filters import CommandStart
 
-bot = Bot(token='7223968806:AAFlYDE4Vys51z5LRlltDkKz2phjv-Mu3d8')
+load_dotenv(find_dotenv())
+
+from handlers.user_private import user_private_router
+
+ALLOWED_UPDATES = ['message, edited_message']
+
+bot = Bot(token=os.getenv('TOKEN'))
 
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer('Start')
-
-
-@dp.message()
-async def echo(message: types.Message):
-    await message.answer(message.text)  # Упомянуть пользователя
-    await message.reply(message.text)
+dp.include_router(user_private_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
 asyncio.run(main())
